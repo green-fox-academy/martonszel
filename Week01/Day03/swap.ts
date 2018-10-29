c29f088155a47478c5949e5a41c4fe61a8fb2919
Márton Szél <marton.szel@gmail.comi>
2018-10-29 14:51:13 +0100

    _

--- Week01/Day03/swap.ts
+++ Week01/Day03/swap.ts
@@ -1,11 +1,14 @@
 'use strict';
 
 // Swap the values of these variables
+
 var a: number = 123;
 var b: number = 526;
 
+
 var a: number = 526;
 var b: number = 123;
+
 
 console.log(a);
 console.log(b);
