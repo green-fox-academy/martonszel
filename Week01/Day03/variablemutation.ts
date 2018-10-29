767da019164f0adc1d63c54186aa50b1efbd783c
Márton Szél <marton.szel@gmail.comi>
2018-10-29 16:27:18 +0100

    update

--- Week01/Day03/variablemutation.ts
+++ Week01/Day03/variablemutation.ts
@@ -55,9 +55,13 @@ if(j%3===0 || j%5===0){console.log(true)}
 else {console.log(false)};
 
 let k: string = 'Apple';
+var i = 1 ;
 // fill the k variable with its content 4 time
-for  (i = 0; i < 5; i++) {
-    console.log(k);
+
+for (i = 0; i < 4; i++) {
+    console.log(k)
 }
+
+
 
 
