1714b6ae5e799394a69b322cdf8798d6f63e55b0
Márton Szél <marton.szel@gmail.comi>
2018-10-29 13:47:54 +0100

    helloclassmates

---
+++ Week01/Day03/helloclassmates.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+ console.log ("Hello, Dávid");
+ console.log ("Hello, Gábor");
+ console.log ("Hello, Rudolf");
