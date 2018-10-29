f8f59b109ebcb743a655ae75e769dfd4c6b4c953
Márton Szél <marton.szel@gmail.comi>
2018-10-29 15:26:33 +0100

    basic

---
+++ Week01/Day03/basicinfi.ts
@@ -0,0 +1,20 @@
+'use strict';
+export{};
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = "Márton Szél";
+console.log(name);
+
+let age = 30 ;
+console.log(age);
+
+let height = 1.86 ;
+console.log(height);
+
+let married = false ;
+console.log(married);
+
