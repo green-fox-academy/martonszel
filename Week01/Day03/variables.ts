fff9685547f2e39f06a53f88c44932b22dabb4cf
Márton Szél <marton.szel@gmail.comi>
2018-10-29 14:31:01 +0100

    variables

---
+++ Week01/Day03/variables.ts
@@ -0,0 +1,28 @@
+// String
+let welcome: string = 'Hello, World';
+console.log(welcome);
+
+// Boolean
+let isAwesome: boolean = true;
+console.log(isAwesome);
+
+// Integer
+let theMeaningOfLifeAndTheUniverseAndEverything: number = 42;
+console.log(theMeaningOfLifeAndTheUniverseAndEverything);
+
+// Floating point number
+let pi: number = 3.141592;
+console.log(pi);
+
+// Assigning and creating a variable, (define its value)
+let number: number = 12;
+console.log(number);
+
+// Mutate a variable, (redefine its value)
+let hook: number = 25;
+console.log(hook);
+
+// Any
+let dynamic: any = 5;
+dynamic = true;
+dynamic = 'strange';
