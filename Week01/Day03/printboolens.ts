2f184ed7e1d29765cc6f6cb10500d1c6dc1df5a6
Márton Szél <marton.szel@gmail.comi>
2018-10-29 13:58:32 +0100

    printboolens

---
+++ Week01/Day03/printboolens.ts
@@ -0,0 +1,25 @@
+
+console.log(true);
+console.log(false);
+
+console.log(!true);
+console.log(!false);
+
+console.log(true && true); 
+console.log(true && false); 
+console.log(false && true); 
+console.log(false && false);
+
+console.log(true || true); 
+console.log(true || false); 
+console.log(false || true); 
+console.log(false || false);
+
+console.log('apple'); 
+console.log('don\'t'); 
+console.log('"Everything you can imagine is real." - Picasso');
+
+console.log('tooth' + 'brush');
+
+console.log('My favorite number is: ' + 12);
+
