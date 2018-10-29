c1b3dcbe890c5dbe3f164f9e8d5bc18ccc67a8aa
Márton Szél <marton.szel@gmail.comi>
2018-10-29 16:56:34 +0100

    update

---
+++ Week01/Day03/cuboid.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+let height = 10
+let lenght = 10
+let depth = 10
+
+console.log(2*(height*lenght)+2*(lenght*depth)+2*(height*depth));
+console.log(height*lenght*depth);
