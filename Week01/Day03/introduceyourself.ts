07e337e053df045f463e497ecdce4515473c9dd5
Márton Szél <marton.szel@gmail.comi>
2018-10-29 14:03:16 +0100

    introduction

---
+++ Week01/Day03/introduceyourself.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+
+
+
+console.log("Márton Szél");
+console.log(30);
+console.log(1.86 );
+
