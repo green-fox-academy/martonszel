b91e3579938c03166fefb75b116b492c1899621e
Márton Szél <marton.szel@gmail.comi>
2018-10-29 15:10:55 +0100

    updated

--- Week01/Day03/bmi.ts
+++ Week01/Day03/bmi.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-let massInKg: number = 81.2;
-let heightInM: number = 1.78;
+let massInKg: number = 100;
+let heightInM: number = 1.86;
 
 // Print the Body mass index (BMI) based on these values
 
