bd1177f25464da7ac66257b512f2f80208023aa1
Márton Szél <marton.szel@gmail.comi>
2018-10-30 12:18:21 +0100

    add summing

---
+++ Week01/Day04/summing.ts
@@ -0,0 +1,11 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+export{};
+
+function sum (a,b,c,d,e) {
+    console.log(a+b+c+d+e);
+} 
+
+sum(1,2,3,4,5);
+
+
