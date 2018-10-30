d2e8dc5356a440801e9cac16a6a6f9722cfec078
Márton Szél <marton.szel@gmail.comi>
2018-10-30 16:44:29 +0100

    sumofall

---
+++ Week01/Day04/sumofall.ts
@@ -0,0 +1,5 @@
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+let ai = [3, 4, 5, 6, 7].reduce((a, b) => a + b);
+console.log(ai);
