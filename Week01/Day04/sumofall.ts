b9ee5ffe7b083dbbe9c768db2d01fcbbd57c4b9b
Márton Szél <marton.szel@gmail.comi>
2018-10-30 17:23:05 +0100

    matrix

--- Week01/Day04/sumofall.ts
+++ Week01/Day04/sumofall.ts
@@ -1,5 +1,10 @@
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
+
 let ai = [3, 4, 5, 6, 7].reduce((a, b) => a + b);
 console.log(ai);
+
+let ai1 = [3, 4, 5, 6, 7]
+console.log(ai1[0]+ai1[1]+ai1[2]+ai1[3]+ai1[4]);
+
