fe0aef124049c5124af1e73ca024e0ed7d19d57b
Márton Szél <marton.szel@gmail.comi>
2018-10-30 15:47:18 +0100

    change

---
+++ Week01/Day04/change elelemnt.ts
@@ -0,0 +1,8 @@
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList = [1, 2, 3, 8, 5, 6].map(function(e) {
+    if(e=== 8)
+    return 4});
+console.log(numList[3]);
