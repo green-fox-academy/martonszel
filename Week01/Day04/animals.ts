9303885b4a15d5787f2ad243d5fec43cff5d25c5
Márton Szél <marton.szel@gmail.comi>
2018-10-30 16:12:10 +0100

    animals

---
+++ Week01/Day04/animals.ts
@@ -0,0 +1,8 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals = ["koal", "pand", "zebr"].forEach(function(e){
+    console.log (e + "a") });
+
+
