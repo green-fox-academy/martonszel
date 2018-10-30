69b7de591aeda1d59f320a263555605280d551ed
Márton Szél <marton.szel@gmail.comi>
2018-10-30 15:17:50 +0100

    swap

---
+++ Week01/Day04/swap.ts
@@ -0,0 +1,9 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc = ["Arthur", "Boe", "Chloe"] ;
+abc.splice (0,3,"Chloe","Boe" ,"Arthur");
+console.log(abc);
+
+
+
