8f0e8c5a4996883e931879c9c56a424c2df02101
Márton Szél <marton.szel@gmail.comi>
2018-10-30 11:33:29 +0100

    add greeter func

---
+++ Week01/Day04/greeter function.ts
@@ -0,0 +1,14 @@
+export{};
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+
+let name = 'Greenfox';
+
+function greet ( greet = "hello" , name = "greenfox"){
+console.log(greet, name);
+}
+
+greet(); 
