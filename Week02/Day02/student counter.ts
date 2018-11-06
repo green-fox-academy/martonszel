cdedc292fb0dc2665953c658acbe6c8a193b407e
Márton Szél <marton.szel@gmail.comi>
2018-11-06 14:06:09 +0100

    update

---
+++ Week02/Day02/student counter.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const students: any[] = [
+  {name: 'Theodor', age: 3, candies: 2},
+  {name: 'Paul', age: 9.5, candies: 2},
+  {name: 'Mark', age: 12, candies: 5},
+  {name: 'Peter', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'George', age: 10, candies: 1}
+];
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+
+function candies(item){
+    return item.candies;
+  }
+  function sum(prev, next){
+    return prev + next;
+  }
+  console.log(students.map(candies).reduce(sum));
+
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
+
---
+++ Week02/Day02/takes longer .ts
@@ -0,0 +1,7 @@
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+
+console.log(quote.slice(0,21)+ 'always takes longer than ' + 'you expect, even when you take into account Hofstadter\'s Law.');
