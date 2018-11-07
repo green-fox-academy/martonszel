5a61207c51d4c2412e0f80c9203afe0f2e0804f0
Márton Szél <marton.szel@gmail.comi>
2018-11-07 14:09:39 +0100

    centeredboxes

---
+++ Week02/Day03/centeredboxes.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a square drawing function that takes 1 parameter:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication
+
+function draw(x) {
+    ctx.fillRect(canvas.width / 2 - x / 2, canvas.height / 2 - x / 2, x, x);
+    ctx.fillStyle = `rgb(${x},200,150)`;
+  }
+  
+  for (let i= 4; i > 1; i--) {
+    draw(50 * i);
+  }
