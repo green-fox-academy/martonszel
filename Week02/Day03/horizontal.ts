0ce07ac228fc8568ca6f105493d7eccfdf3237ed
Márton Szél <marton.szel@gmail.comi>
2018-11-07 12:00:27 +0100

    horizontal

---
+++ Week02/Day03/horizontal.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// create a line drawing function that takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// draw 3 lines with that function. Use loop for that.
+
+function draw (x,y){
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'green';
+    ctx.moveTo(10, 50);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+for ( let i=0 ; i<3 ; i++){
+    draw(i* 50,i);
+}
