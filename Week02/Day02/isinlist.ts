edb1850add6e88b12de372f6a113d94c5e804c82
Márton Szél <marton.szel@gmail.comi>
2018-11-08 09:56:34 +0100

    isinlist

--- Week02/Day02/isinlist.ts
+++ Week02/Day02/isinlist.ts
@@ -3,21 +3,22 @@
 // Create a function that accepts 'listOfNumbers' as an input
 // it should return "true" if it contains all, otherwise "false"
 
-let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
-let newfr = listOfNumbers.includes(4);
-
+let listOfNumbers: number[] = [2, 4 , 6, 8, 10, 12, 14, 16];
+let diviser = 4;
 
 
 function checkNums(listOfNumbers) {
     for (let i = 0; i < listOfNumbers.length; i++) {
-        if (listOfNumbers[i] % 4 === 0) {
+        if (listOfNumbers.includes (4||8||12||16) ) {
             return true
         }
-        diviser += 4
-        else { return false }
-    }
-}
 
+        else {
+            return false
+        }
+    }
+    
+}
 console.log(checkNums(listOfNumbers));
 
 export = checkNums;
