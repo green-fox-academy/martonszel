ef0c03f5b3ecfc4ec0739f319c05785d29a12646
Márton Szél <marton.szel@gmail.comi>
2018-11-09 10:27:00 +0100

    lineplay

---
+++ Week02/Day05/lineplay.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+for (let i = 0 ; i < 20 ; i ++){
+
+    ctx.beginPath();
+    ctx.moveTo(i*30 , 0); 
+    ctx.lineTo (canvas.width,i*20) ; 
+    ctx.strokeStyle = "purple" ; 
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0 , i*20); 
+    ctx.lineTo (i*30,canvas.height) ; 
+    ctx.strokeStyle = "green" ; 
+    ctx.stroke();
+
+
+}
+
+
+
