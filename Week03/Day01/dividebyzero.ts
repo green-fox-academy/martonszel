1caad59ffa9c7018adbbe7231fbc8ff27896031e
Márton Szél <marton.szel@gmail.comi>
2018-11-12 12:05:32 +0100

    add dividebyzero

---
+++ Week03/Day01/dividebyzero.ts
@@ -0,0 +1,21 @@
+export{};
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+const fs = require('fs');
+
+function divideBy (a:number): number {
+if ( a === 0) {
+    throw new Error ('fail');
+}
+return a / 10 ; 
+}
+
+try {
+    console.log(divideBy(0));
+  } catch (e) {
+    console.log(e.message);
+  } 
