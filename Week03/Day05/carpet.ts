07ad47370d4d133ae74f3d2c5a6552faad8b5a70
Márton Szél <marton.szel@gmail.comi>
2018-11-18 14:35:01 +0100

    carpet

--- Week03/Day05/carpet.ts
+++ Week03/Day05/carpet.ts
@@ -2,4 +2,11 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
- 
+
+function fib(n) {
+  if (n < 2) {
+    return n
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
