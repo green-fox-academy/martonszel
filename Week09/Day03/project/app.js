cefec841fd169c333581b891b37dfa591e0f6ae0
Márton Szél <marton.szel@gmail.comi>
2018-12-19 13:37:22 +0100

    feat(frontend project): solve second task Greeter

--- Week09/Day03/project/app.js
+++ Week09/Day03/project/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
- app.get('/doubling', (req, res) => {
+app.get('/doubling', (req, res) => {
   const { input } = req.query;
   if (input) {
     res.json({
@@ -23,6 +23,27 @@ app.get('/', (req, res) => {
     });
   }
 })
+
+app.get('/greeter', (req, res) => {
+  if (req.query.name && req.query.title) {
+    res.json({
+     welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+    });
+  } else if (!req.query.name) {
+    res.json({
+      error: 'Please provide a name!',
+    });
+  } else if (!req.query.title) {
+    res.json({
+      error: 'Please provide a title!',
+    });
+  } else {
+    res.json({
+      error: `Please provide a name and a title!`,
+    });
+  }
+})
+
 
 
 app.listen(PORT, () => {
